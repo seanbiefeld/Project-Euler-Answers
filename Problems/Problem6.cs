e6608a15bf799fdfed77637936ce3da1816d7f98
seanbiefeld <sbiefeld@gmail.com>
2011-09-07 11:33:18 -0500

    adding problem 6

---
+++ Problems/Problem6.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ProjectEuler.Problems
+{
+	public class Problem6 : IProblem
+	{
+		/// <summary>
+		/// Find the difference between the sum of the squares
+		/// of the first one hundred natural numbers and the square of the sum.
+		/// </summary>
+		/// <returns></returns>
+		public string Solve()
+		{
+			var sumOfTheSquares = 0d;
+			var sumOfNaturalNumbers = 0d;
+
+			for (double i = 1; i <= 100; i++)
+			{
+				sumOfTheSquares += Math.Pow(i, 2d);
+				sumOfNaturalNumbers +=i;
+			}
+
+			var squareOfTheSum = Math.Pow(sumOfNaturalNumbers, 2d);
+			var difference = squareOfTheSum - sumOfTheSquares;
+
+			return difference.ToString();
+		}
+	}
+}
